25ffd38988062bb11a51c7f393058db0aa3224af
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2022-08-28 16:44:42 -0600

    Add files via upload

---
+++ tilesets/enemies.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="enemies" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../assets/enemy0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../assets/enemy1.png"/>
+ </tile>
+</tileset>
---
+++ tilesets/platform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="platform" tilewidth="64" tileheight="64" tilecount="72" columns="12">
+ <image source="../../../../platform_metroidvania asset pack v1.01/tiles and background_foreground (new)/tileset_64x64(new).png" width="768" height="384"/>
+</tileset>
